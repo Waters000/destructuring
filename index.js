0e00cbd1b81d567f7923f8df3684e533be53e414
Chris Waters <floridarealty001@gmail.com>
2022-02-17 21:10:38 -0500

    updated

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const nodejs = {
   type: 'JavaScript runtime environment'
 };
 // same as 
-// const {name, type} = nodejs;
+ const {name, type} = nodejs;
 
+
+console.log(name);
+console.log(type);
 
 // TODO: Destructure the object 'nodejs'
 
-console.log(nodejs.name); // <= Node.js
-console.log(nodejs.type); // <= JavaScript runtime environment
+// console.log(nodejs.name); // <= Node.js
+// console.log(nodejs.type); // <= JavaScript runtime environment
 
 // 2. Nested Object
 const js = {
@@ -29,23 +32,28 @@ const js = {
   }
 };
 
+const {framework1, framework2} = js.tools.frameworks;
+
+console.log(framework1);
+console.log(framework2);
+
 // TODO: Destructure the nested object 'js'
 
-console.log(js.tools.frameworks.framework1); // <= AngularJS
-console.log(js.tools.frameworks.framework2); // <= Vue.js
+// console.log(js.tools.frameworks.framework1); // <= AngularJS
+// console.log(js.tools.frameworks.framework2); // <= Vue.js
 
 // 3. Arrays
 const languages = ['HTML', 'CSS', 'JavaScript'];
+
+const [markup, style, scripting] = languages
 
 // TODO: Destructure the array 'languages'
 
 //console.log(languages.length); // <= HTML CSS JavaScript
 //languages.forEach(console.log)
- console.log(languages[0], languages[1], languages[2])
+//  console.log(languages[0], languages[1], languages[2])
+// console.log(languages[0]); // <= HTML
 
 
-
-
-
-
-console.log(languages[0]); // <= HTML
+console.log(markup, style, scripting);
+console.log(markup);
